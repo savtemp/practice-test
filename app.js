83085915284528a1bad032b63fff8bc6ed45e574
Jake Overall <joverall22@gmail.com>
2021-09-21 01:12:18 -0600

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+const user = {
+  name: 'Sherlock Holmes',
+  address: {
+    street: '221B Baker Street',
+    city: 'London'
+  }
+}
+
+const cases = [
+  { title: 'The Hound of the Baskervilles', releaseYear: 1901 },
+  { title: 'The Adventure of the Empty House', releaseYear: 1903 },
+  { title: 'The Adventure of the Norwood Builder', releaseYear: 1903 },
+  { title: 'The Adventure of the Dancing Men', releaseYear: 1903 },
+  { title: 'The Adventure of the Solitary Cyclist', releaseYear: 1904 }
+]
+
+// YOUR JS CODE
+
+
